c1f79efadba407d4605ab32a05410e9954eb5a56
TheZodo <malaizyogmuzumala@gmail.com>
2021-12-27 23:38:41 +0200

    Haii again

--- reactZodo/zodo/src/Pages/HomePage.js
+++ reactZodo/zodo/src/Pages/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Text from '../components/text'
 const HomePage = () => {
-    return ( <
-        div >
-        <
-        Text type = 'heading' >
-        I 'm a simp <br/> ANd I love it < /
-        Text > <
-        /div>
-    )
-}
-
+        return ( <
+            div >
+            <
+            Text type = 'heading' >
+            I 'm a simp <br/> ANd I love it < /
+            Text > <
+            /div>
+        )
+    }
+    // hey there, just updating</Text>
 export default HomePage
