32d01c81e0a82e5b9bb3ae7d23a6b04572cbb2fe
TheZodo <malaizyogmuzumala@gmail.com>
2022-01-31 14:36:35 +0200

    Latest

--- tutorial-video/src/index.js
+++ tutorial-video/src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import reactDom from 'react-dom';
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App";
 
 function Greeting() {
-    return <h4 > this is Zodo and this is my first component < /h4>;
+  return <App />;
 }
 
-reactDom.render( < Greeting / > , document.getElementById('root'));
+const Book = () => {
+  return (
+    <div>
+      <IconButton src={<House />} />
+      <Image></Image>
+      <Title />
+      <Author />
+    </div>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41n9-p6-PpL.jpg"
+      alt="There"
+      width={"15%"}
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>Reminders of him</h1>;
+};
+const Author = () => <h4>Colleen Hoover</h4>;
+
+reactDom.render(<Greeting />, document.getElementById("root"));
