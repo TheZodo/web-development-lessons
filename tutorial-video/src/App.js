e7a03b20ed42d565e2ba94ee408155a982847361
TheZodo <malaizyogmuzumala@gmail.com>
2022-02-18 20:53:32 +0200

    Portfolio Site

--- tutorial-video/src/App.js
+++ tutorial-video/src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
 
-const App = () => {
-  return <div></div>;
+function App() {
+  return (
+    <div>
+      <Book />
+    </div>
+  );
+}
+
+const Book = () => {
+  return (
+    <div>
+      <Image></Image>
+      <Title />
+      <Author />
+    </div>
+  );
 };
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41n9-p6-PpL.jpg"
+      alt="There"
+      width={"15%"}
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>Reminders of him</h1>;
+};
+const Author = () => <h4>Colleen Hoover</h4>;
 
 export default App;
--- tutorial-video/src/index.js
+++ tutorial-video/src/index.js
@@ -1,35 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
 import App from "./App";
+import "./index.css";
 
-function Greeting() {
-  return <App />;
-}
-
-const Book = () => {
-  return (
-    <div>
-      <IconButton src={<House />} />
-      <Image></Image>
-      <Title />
-      <Author />
-    </div>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/41n9-p6-PpL.jpg"
-      alt="There"
-      width={"15%"}
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>Reminders of him</h1>;
-};
-const Author = () => <h4>Colleen Hoover</h4>;
-
-reactDom.render(<Greeting />, document.getElementById("root"));
+reactDom.render(<App />, document.getElementById("root"));
